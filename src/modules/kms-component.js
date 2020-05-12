31974cf41f6ed0b2cbeda3c66ab11e3a1f5a3b6f
Yuli Hardina <yulihardinap@gmail.com>
2020-05-12 22:27:48 +0800

    kms-component

--- src/modules/kms-component.js
+++ src/modules/kms-component.js
@@ -8,109 +8,8 @@ import Form from 'antd/lib/form/Form';
 const { Content } = Layout; // membuat konstanta content 
   
 class KmsComponent extends Component{
-    state = { visible: false }
-    showModal = () => {
-        this.setState({
-        visible: true,
-        });
-    }
-    handleChange = (e) => {
-        let target = e.target.name;
-        let value = e.target.value;
-        this.setState({
-            [target]: value
-        })
-    }
-    handleOk = (e) => {
-        console.log(e);
-        this.setState({
-          visible: false,
-        });
-      }
-      handleCancel = (e) => {
-        console.log(e);
-        this.setState({
-          visible: false,
-        });
-      }
-
     render(){
-        const columns = [{
-            title: 'Nama Anak',
-            dataIndex: 'nama_anak',
-            key: 'nama_anak',
-            render: text => <a href="#">{text}</a>,
-          }, {
-            title: 'Usia',
-            dataIndex: 'usia_anak',
-            key: 'usia_anak',
-          },
-          {
-            title: 'Berat Badan',
-            dataIndex: 'bb',
-            key: 'bb',
-          }, {
-            title: 'Tinggi Badan',
-            dataIndex: 'tb',
-            key: 'tb',
-          }, {
-            title: 'Instansi',
-            dataIndex: 'instansi',
-            key: 'instansi',
-          }, {
-            title: 'Lokasi',
-            dataIndex: 'lokasi',
-            key: 'lokasi',
-          },{
-            title: 'Tanggal',
-            dataIndex: 'tanggal',
-            key: 'tanggal',
-          }, {
-            title: 'Action',
-            key: 'action',
-            render: (text, record) => (
-              <span>
-                <a href="#">Edit</a>
-                <span className="ant-divider" />
-                <a href="#">Delete</a>
-                <span className="ant-divider" />
-                <a href="#" className="ant-dropdown-link">
-                  {/* More actions <Icon type="down" /> */}
-                </a>
-              </span>
-            ),
-        }];
-        
-        const data = [{
-            key: '1',
-            nama_anak: 'John Brown',
-            usia_anak: 3,
-            bb: 10,
-            tb: 120,
-            instansi: 'Puskesmas Danurejan I',
-            lokasi: 'Danurejan',
-            tanggal: '12-12-2019'
-          }, {
-            key: '1',
-            nama_anak: 'Winsie Wiradi',
-            usia_anak: 3,
-            bb: 20,
-            tb: 120,
-            instansi: 'Puskesmas Danurejan I',
-            lokasi: 'Danurejan',
-            tanggal: '12-12-2019'
-          }, {
-            key: '1',
-            nama_anak: 'Malika Brown',
-            usia_anak: 2,
-            bb: 10,
-            tb: 100,
-            instansi: 'Puskesmas Danurejan I',
-            lokasi: 'Danurejan',
-            tanggal: '12-12-2019'
-          }, 
-        ];
-
+        const {columns,anak,showModal,initialData,handleOk,handleChange,handleCancel,handleCancelEdit,handleOkEdit} = this.props
         return(
             <Layout className="landing-container">
                 <Content style={{ 
@@ -121,39 +20,39 @@ class KmsComponent extends Component{
                     }}
                 > 
                     <Row className="section-container">
-                        <Col lg={24}>
-                            <div className="text-soft-blue title-big bold discover-container">
-                            Data KMS Anak
-                            </div>
-                        </Col>
-                        <Col lg={24}>
-                            <div className="text-black description-discover">
-                            Platform pengisian data KMS oleh Admin Posyandu.
-                            </div>
-                        </Col>
-                        <Col lg={24}>
-                            <hr style={{
-                                minHeight: 8,
-                                backgroundColor: '#7d5e2a',
-                                border: 'none',
-                                maxWidth: 150,
-                                borderRadius: 26,
-                            }}/>
-                        </Col>                    
-                        <Col lg={24} className="mt-20">
-                            <Button key="tambah_data" className="editable-add-btn" type="primary" onClick={()=>this.showModal()}>Tambah Data
-                            </Button>
-                            <br></br>
-                            <br></br>
-                            <Table columns={columns} dataSource={data} />
-                        </Col>
-                    </Row>
+                            <Col lg={24}>
+                                <div className="text-soft-blue title-big bold discover-container">
+                                Data KMS Anak
+                                </div>
+                            </Col>
+                            <Col lg={24}>
+                                <div className="text-black description-discover">
+                                Platform pengisian data KMS oleh Admin Posyandu.
+                                </div>
+                            </Col>
+                            <Col lg={24}>
+                                <hr style={{
+                                    minHeight: 8,
+                                    backgroundColor: '#7d5e2a',
+                                    border: 'none',
+                                    maxWidth: 150,
+                                    borderRadius: 26,
+                                }}/>
+                            </Col>                    
+                            <Col lg={24} className="mt-20">
+                                <Button key="tambah_data" className="editable-add-btn" type="primary" onClick={()=>showModal()}>Tambah Data
+                                </Button>
+                                <br></br>
+                                <br></br>
+                                <Table columns={columns} dataSource={anak} />
+                            </Col>
+                        </Row>
                 </Content>
                 <Modal
                     title="Tambah Kategori"
-                    visible={this.state.visible}
-                    onOk={this.handleOk}
-                    onCancel={this.handleCancel}
+                    visible={initialData.visible}
+                    onOk={handleOk}
+                    onCancel={handleCancel}
                 >
                 <Form>
                     <Row>
@@ -163,11 +62,11 @@ class KmsComponent extends Component{
                             </div>
                             <div>
                                 <InputForm 
-                                    name='nama_anak'
+                                    name='name'
                                     placeholder="Masukkan nama anak..."
                                     className="input-event mt-5 mb-20"
-                                    onChange={this.handleChange}
-                                    value={this.state.nama_anak}
+                                    onChange={handleChange}
+                                    value={initialData.name}
                                 />
                             </div>
                         </Col>
@@ -180,11 +79,11 @@ class KmsComponent extends Component{
                             </div>
                             <div>
                                 <InputForm 
-                                    name='usia_anak'
+                                    name='age'
                                     placeholder="Masukkan usia anak..."
                                     className="input-event mt-5 mb-20"
-                                    onChange={this.handleChange}
-                                    value={this.state.usia_anak}
+                                    onChange={handleChange}
+                                    value={initialData.age}
                                 />
                             </div>
                         </Col>
@@ -197,11 +96,11 @@ class KmsComponent extends Component{
                             </div>
                             <div>
                                 <InputForm 
-                                    name='bb'
+                                    name='weight'
                                     placeholder="Masukkan berat badan anak..."
                                     className="input-event mt-5 mb-20"
-                                    onChange={this.handleChange}
-                                    value={this.state.bb}
+                                    onChange={handleChange}
+                                    value={initialData.weight}
                                 />
                             </div>
                         </Col>
@@ -214,11 +113,153 @@ class KmsComponent extends Component{
                             </div>
                             <div>
                                 <InputForm 
-                                    name='tb'
+                                    name='height'
                                     placeholder="Masukkan tinggi badan anak..."
                                     className="input-event mt-5 mb-20"
-                                    onChange={this.handleChange}
-                                    value={this.state.tb}
+                                    onChange={handleChange}
+                                    value={initialData.height}
+                                />
+                            </div>
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col lg={24} md={24} sm={24}>
+                            <div>
+                                <span className="auth-input-label text-black">Lokasi</span>
+                            </div>
+                            <div>
+                                <InputForm 
+                                    name='location'
+                                    placeholder="Masukkan lokasi ..."
+                                    className="input-event mt-5 mb-20"
+                                    onChange={handleChange}
+                                    value={initialData.location}
+                                />
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col lg={24} md={24} sm={24}>
+                            <div>
+                                <span className="auth-input-label text-black">Institusi</span>
+                            </div>
+                            <div>
+                                <InputForm 
+                                    name='institution'
+                                    placeholder="Masukkan institusi..."
+                                    className="input-event mt-5 mb-20"
+                                    onChange={handleChange}
+                                    value={initialData.institution}
+                                />
+                            </div>
+                        </Col>
+                    </Row>
+                </Form>
+            </Modal>
+            <Modal
+                    title="Edit Kategori"
+                    visible={initialData.visible_edit}
+                    onOk={handleOkEdit}
+                    onCancel={handleCancelEdit}
+                >
+                <Form>
+                    <Row>
+                        <Col lg={24} md={24} sm={24}>
+                            <div>
+                                <span className="auth-input-label text-black">Nama Anak</span>
+                            </div>
+                            <div>
+                                <InputForm 
+                                    name='name_edit'
+                                    placeholder="Masukkan nama anak..."
+                                    className="input-event mt-5 mb-20"
+                                    onChange={handleChange}
+                                    value={initialData.name_edit}
+                                />
+                            </div>
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col lg={24} md={24} sm={24}>
+                            <div>
+                                <span className="auth-input-label text-black">Usia</span>
+                            </div>
+                            <div>
+                                <InputForm 
+                                    name='age_edit'
+                                    placeholder="Masukkan usia anak..."
+                                    className="input-event mt-5 mb-20"
+                                    onChange={handleChange}
+                                    value={initialData.age_edit}
+                                />
+                            </div>
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col lg={24} md={24} sm={24}>
+                            <div>
+                                <span className="auth-input-label text-black">Berat Badan</span>
+                            </div>
+                            <div>
+                                <InputForm 
+                                    name='weight_edit'
+                                    placeholder="Masukkan berat badan anak..."
+                                    className="input-event mt-5 mb-20"
+                                    onChange={handleChange}
+                                    value={initialData.weight_edit}
+                                />
+                            </div>
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col lg={24} md={24} sm={24}>
+                            <div>
+                                <span className="auth-input-label text-black">Tinggi Badan</span>
+                            </div>
+                            <div>
+                                <InputForm 
+                                    name='height_edit'
+                                    placeholder="Masukkan tinggi badan anak..."
+                                    className="input-event mt-5 mb-20"
+                                    onChange={handleChange}
+                                    value={initialData.height_edit}
+                                />
+                            </div>
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col lg={24} md={24} sm={24}>
+                            <div>
+                                <span className="auth-input-label text-black">Lokasi</span>
+                            </div>
+                            <div>
+                                <InputForm 
+                                    name='location_edit'
+                                    placeholder="Masukkan lokasi ..."
+                                    className="input-event mt-5 mb-20"
+                                    onChange={handleChange}
+                                    value={initialData.location_edit}
+                                />
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col lg={24} md={24} sm={24}>
+                            <div>
+                                <span className="auth-input-label text-black">Institusi</span>
+                            </div>
+                            <div>
+                                <InputForm 
+                                    name='institution_edit'
+                                    placeholder="Masukkan institusi..."
+                                    className="input-event mt-5 mb-20"
+                                    onChange={handleChange}
+                                    value={initialData.institution_edit}
                                 />
                             </div>
                         </Col>
